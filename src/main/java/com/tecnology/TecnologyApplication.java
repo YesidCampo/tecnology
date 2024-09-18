ee00771c04e05324be9dfd4fba85dfddf4c18db4
YesidCampo <yesidcampogonzalez@gmail.com>
2024-09-18 14:24:24 -0500

    Mi first commit

---
+++ src/main/java/com/tecnology/TecnologyApplication.java
@@ -0,0 +1,13 @@
+package com.tecnology;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TecnologyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TecnologyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tecnology/TecnologyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tecnology;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TecnologyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
